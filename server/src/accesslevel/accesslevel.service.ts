39d6aa1a6c39b42b6e7d79dbb248e770835ed6df
rklasss <ogclubbody@gmail.com>
2025-05-18 18:57:37 +0700

    feat(accesslevel): add func for create

--- server/src/accesslevel/accesslevel.service.ts
+++ server/src/accesslevel/accesslevel.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { AccessLevel } from './accesslevel.model';
 
 @Injectable()
-export class AccesslevelService {}
+export class AccesslevelService {
+    constructor(@InjectModel(AccessLevel) private accessLevelRepository: typeof AccessLevel) {}
+
+    async create(name: string): Promise <AccessLevel> {
+        const existing = await this.accessLevelRepository.findOne({where: {name}});
+        return this.accessLevelRepository.create({name});
+    }
+}
