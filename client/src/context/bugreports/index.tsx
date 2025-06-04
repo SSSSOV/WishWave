89411519f0257a251f55e73d080ad2b1ba185b31
Aleksandr <98227655+SSSSOV@users.noreply.github.com>
2025-06-04 13:46:13 +0700

    feat(user): user can delete self and admin can delete other

--- client/src/context/bugreports/index.tsx
+++ client/src/context/bugreports/index.tsx
@@ -10,6 +10,7 @@ export const $bugreports = createStore<IBugreport[] | null>(null)
 export const $stats = createStore<IStats | null>(null)
 
 export const handleFetchBugreports = createEvent()
+export const handleDeleteBugreport = createEvent<number>()
 export const handleFetchStats = createEvent()
 export const handleCreateBugreport = createEvent<ICreateBugreport>()
 
@@ -57,6 +58,24 @@ export const createBugreportFx = createEffect(async (params: ICreateBugreport) =
     throw error
   }
 })
+export const deleteBugreportFx = createEffect(async (id: number) => {
+  const token = localStorage.getItem("auth")
+
+  try {
+    const { data } = await api.delete(`/api/bugreport/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+
+    if (data.warningMessage) {
+      toast.error(data.warningMessage)
+      return null
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) toast.error(error.response?.data.message)
+    else toast.error("Произошла непредвиденная ошибка: " + error)
+    throw error
+  }
+})
 export const fetchStatsFx = createEffect(async () => {
   const token = localStorage.getItem("auth")
 
@@ -88,3 +107,5 @@ $stats.on(fetchStatsFx.doneData, (_, result) => (result ? result : null))
 sample({ clock: handleCreateBugreport, target: createBugreportFx })
 sample({ clock: handleFetchBugreports, target: fetchBugreportsFx })
 sample({ clock: handleFetchStats, target: fetchStatsFx })
+sample({ clock: handleDeleteBugreport, target: deleteBugreportFx })
+sample({ clock: deleteBugreportFx.done, target: handleFetchBugreports })
